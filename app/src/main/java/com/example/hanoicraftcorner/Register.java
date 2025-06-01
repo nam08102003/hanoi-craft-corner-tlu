448e2708af2327de01f93ead668dab63954e63ac
hungbeoqua321 <2151060248@e.tlu.edu.vn>
2025-06-01 22:43:36 +0700

    update small things

--- app/src/main/java/com/example/hanoicraftcorner/Register.java
+++ app/src/main/java/com/example/hanoicraftcorner/Register.java
@@ -1,6 +1,9 @@
 package com.example.hanoicraftcorner;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Register extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class Register extends AppCompatActivity {
+    ImageButton backButton;
+    EditText usernameEditText,
+             emailEditText,passwordEditText;
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,21 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        backButton = findViewById(R.id.imageButton);
+        usernameEditText = findViewById(R.id.inpUsername);
+        emailEditText = findViewById(R.id.inpEmail);
+        passwordEditText = findViewById(R.id.inpPassword);
+        registerButton = findViewById(R.id.registerButton);
+
+        backButton.setOnClickListener(v -> {
+//            Update the code to handle back navigation
+        });
+        registerButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString();
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            // Handle registration logic here
         });
     }
 }
