e4ccb83aaa5714714cd2f81b9e089f07e2d99d66
linhnng <2251061824@e.tlu.edu.vn>
2025-06-08 00:15:43 +0700

    update login feature

---
+++ app/src/main/java/com/example/hanoicraftcorner/LoginActivity.java
@@ -0,0 +1,78 @@
+package com.example.hanoicraft;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    Button logIn, regisArtisan;
+    EditText email, password;
+    TextView signUp,forgotPassword;
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        logIn = findViewById(R.id.btn_login);
+        regisArtisan = findViewById(R.id.btn_regis_artisan);
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        signUp = findViewById(R.id.sign_up);
+        forgotPassword = findViewById(R.id.forgot_password);
+        auth = FirebaseAuth.getInstance();
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logInUser();
+            }
+        });
+    }
+
+    private void logInUser() {
+        String emailText = email.getText().toString().trim();
+        String passwordText = password.getText().toString().trim();
+
+        if (emailText.isEmpty() || passwordText.isEmpty()) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if( passwordText.length() < 6) {
+            Toast.makeText(this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        auth.signInWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Login successful
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // Login failed
+                            Toast.makeText(LoginActivity.this, "Login failed: " + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+        });
+
+    }
+
+}
+
+
