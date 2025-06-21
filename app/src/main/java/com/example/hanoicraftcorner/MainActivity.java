7780f251bc7f5486764f4e02a93cbc5146a101c7
Lê Đức Nam <ducnamle81003@gmail.com>
2025-06-21 23:00:45 +0700

    Revert "phamquochung/feature/Register"

--- app/src/main/java/com/example/hanoicraftcorner/MainActivity.java
+++ app/src/main/java/com/example/hanoicraftcorner/MainActivity.java
@@ -1,8 +1,6 @@
-package com.example.hanoicraftcorner.resetpassword;
+package com.example.hanoicraftcorner;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.hanoicraftcorner.main.MainActivity;
-import com.example.hanoicraftcorner.R;
-
-public class ResetPasswordSuccessActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_reset_password_success);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button btnSubmit = findViewById(R.id.btn_submit);
-        btnSubmit.setOnClickListener(v -> {
-            Intent loginIntent = new Intent(ResetPasswordSuccessActivity.this, MainActivity.class);
-            startActivity(loginIntent);
         });
     }
 }
